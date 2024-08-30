2b35936f4d464dcf550fa8a92eb04af9c158c969
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2024-08-30 13:02:27 -0600

    feat(dental-clinic-backend): add new methods to dentist repository

--- src/main/java/com/dh/Dental/Clinic/controller/DentistController.java
+++ src/main/java/com/dh/Dental/Clinic/controller/DentistController.java
@@ -4,6 +4,7 @@ import com.dh.Dental.Clinic.entity.Dentist;
 
 import com.dh.Dental.Clinic.service.IDentistService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
@@ -21,43 +22,53 @@ public class DentistController {
     private final IDentistService dentistService;
 
     public DentistController(IDentistService dentistService) {
-
         this.dentistService = dentistService;
-
     }
 
     @PostMapping("/save")
-
     public ResponseEntity<Dentist> saveDentist(@RequestBody Dentist dentist) {
-
         return ResponseEntity.ok(dentistService.saveDentist(dentist));
-
     }
 
     @GetMapping("/find/{id}")
-
     public ResponseEntity<Dentist> findDentistById(@PathVariable Integer id) {
-
         Optional<Dentist> dentist = dentistService.findDentistById(id);
-
         if (dentist.isPresent()) {
-
             return ResponseEntity.ok(dentist.get());
-
         } else {
-
             return ResponseEntity.notFound().build();
-
         }
-
     }
 
     @GetMapping("/all")
-
     public ResponseEntity<List<Dentist>> findAllDentists() {
-
         return ResponseEntity.ok(dentistService.findAllDentists());
+    }
 
+    @PutMapping ("/update")
+    public ResponseEntity<String> updateDentist(@RequestBody Dentist dentist) {
+
+        Optional<Dentist> existingDentist = dentistService.findDentistById(dentist.getId());
+        if (existingDentist.isPresent()) {
+            dentistService.updateDentist(dentist);
+            String jsonResponse = "{\"message\": \"The dentist was updated\"}";
+            return ResponseEntity.ok(jsonResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity<String> deleteDentist(@PathVariable Integer id) {
+        Optional<Dentist> existingDentist = dentistService.findDentistById(id);
+
+        if (existingDentist.isPresent()) {
+            dentistService.deleteDentist(id);
+            String jsonResponse = "{\"message\": \"The dentist was deleted\"}";
+            return ResponseEntity.ok(jsonResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
--- src/main/java/com/dh/Dental/Clinic/service/impl/DentistService.java
+++ src/main/java/com/dh/Dental/Clinic/service/impl/DentistService.java
@@ -2,6 +2,7 @@ package com.dh.Dental.Clinic.service.impl;
 
 import com.dh.Dental.Clinic.entity.Dentist;
 
+import com.dh.Dental.Clinic.entity.Patient;
 import com.dh.Dental.Clinic.repository.IDentistRepository;
 
 import com.dh.Dental.Clinic.service.IDentistService;
@@ -13,39 +14,36 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-
 public class DentistService implements IDentistService {
 
     private final IDentistRepository dentistRepository;
 
     public DentistService(IDentistRepository dentistRepository) {
-
         this.dentistRepository = dentistRepository;
-
     }
 
     @Override
-
     public Dentist saveDentist(Dentist dentist) {
-
         return dentistRepository.save(dentist);
-
     }
 
     @Override
-
     public Optional<Dentist> findDentistById(Integer id) {
-
         return dentistRepository.findById(id);
-
     }
 
     @Override
-
     public List<Dentist> findAllDentists() {
-
         return dentistRepository.findAll();
-
     }
 
+    @Override
+    public void updateDentist(Dentist dentist) {
+        dentistRepository.save(dentist);
+    }
+
+    @Override
+    public void deleteDentist(Integer id) {
+        dentistRepository.deleteById(id);
+    }
 }
